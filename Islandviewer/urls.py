2edf0a700287c14c008e27c4f0a4ae669b65cb0b
lairdm <lairdm@sfu.ca>
2014-01-15 16:36:17 -0800

    change paths depending if its dev or not

--- Islandviewer/urls.py
+++ Islandviewer/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import patterns, include, url
+import settings.env
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('',
-    url(r'^islandviewer/', include('webui.urls'))
+if settings.env.DEV_ENV:
+    urlpatterns = patterns('',
+        url(r'^islandviewer/', include('webui.urls'))
       
     # Examples:
     # url(r'^$', 'Islandviewer.views.home', name='home'),
@@ -16,4 +18,9 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-)
+    )
+else:
+    urlpatterns = patterns('',
+        url(r'^/', include('webui.urls'))
+    )
+    
