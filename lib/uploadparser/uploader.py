9584e889c2bad357b4d3eea00399a21331f39776
lairdm <lairdm@sfu.ca>
2014-01-17 10:51:23 -0800

    Wrong paths

--- lib/uploadparser/uploader.py
+++ lib/uploadparser/uploader.py
@@ -60,7 +60,7 @@ class GenomeParser():
             output = subprocess.check_output(upload_script.split(), universal_newlines=True)
         except subprocess.CalledProcessError as e:
             print "Error submitting: " + str(e.returncode), e.output
-            raise Exception("Error submitting analysis", "{0}".format(e.output))
+            raise Exception("Error submitting analysis", "{0}".format(e.output), "{0}".format(upload_script))
         else:
             print "Successfully submitted " + output
 #            return 20
