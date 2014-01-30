2732a68014509bfd5833c0c06295dc6fb0a31d9b
lairdm <lairdm@sfu.ca>
2014-01-30 14:10:41 -0800

    prod vs testing

--- Islandviewer/settings/paths.py
+++ Islandviewer/settings/paths.py
@@ -11,8 +11,8 @@ if env.PROD_ENV:
     GENOME_SUBMISSION_SCRIPT = "/data/Modules/iv-backend/islandviewer/bin/submit_uploaded_genome.pl -c /data/Modules/iv-backend/islandviewer/etc/islandviewer.config -f {filename} -n \"{genome_name}\" -l /data/Modules/iv-backend/islandviewer/etc/logger.upload.conf 2>/dev/null"
     PIPELINE_PATH = "/data/Resources/MetaScheduler/pipeline"
 elif env.TEST_ENV:
-    GENOME_UPLOAD_PATH = "/data/Modules/iv-backend/islandviewer/custom_genomes/tmp/"
-    GENOME_SUBMISSION_SCRIPT = "/data/Modules/iv-backend/islandviewer/bin/submit_uploaded_genome.pl -c /data/Modules/iv-backend/islandviewer/etc/islandviewer.config -f {filename} -n \"{genome_name}\" -l /data/Modules/iv-backend/islandviewer/etc/logger.upload.conf 2>/dev/null"
+    GENOME_UPLOAD_PATH = "/data/Modules/iv-backend/islandviewer_dev/custom_genomes/tmp/"
+    GENOME_SUBMISSION_SCRIPT = "/data/Modules/iv-backend/islandviewer_dev/bin/submit_uploaded_genome.pl -c /data/Modules/iv-backend/islandviewer/etc/islandviewer.config -f {filename} -n \"{genome_name}\" -l /data/Modules/iv-backend/islandviewer/etc/logger.upload.conf 2>/dev/null"
     PIPELINE_PATH = "/data/Resources/MetaScheduler/pipeline"
 else:
     GENOME_UPLOAD_PATH = "/home/lairdm/islandviewer/custom_genomes/tmp/"
--- Islandviewer/settings/uploader.py
+++ Islandviewer/settings/uploader.py
@@ -7,6 +7,7 @@ elif env.TEST_ENV:
     ISLANDVIEWER_HOST = 'controlbk'
     ISLANDVIEWER_PORT = 8212
 else:
+    # DEV_ENV
     ISLANDVIEWER_HOST = 'localhost'
     ISLANDVIEWER_PORT = 8211
 
