229eefb4c6b528da0138ed44a26c728383de56c3
Matthew Laird <lairdm@gmail.com>
2015-09-11 15:14:08 -0700

    Don't remove virulence table!

--- webui/models.py
+++ webui/models.py
@@ -478,6 +478,15 @@ class Version(models.Model):
         managed = False
         db_table = 'version'
 
+class Virulence(models.Model):
+    protein_accnum = models.CharField(max_length=18,primary_key=True)
+    external_id = models.CharField(max_length=18)
+    source = models.CharField(max_length=4, blank=True)
+    type = models.CharField(max_length=20, blank=False)
+    class Meta:
+        managed = False
+        db_table = 'virulence'
+ 
 
 
 
