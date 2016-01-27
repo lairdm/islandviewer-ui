8c8b808716bf9094e36138e070e761499b199d4a
adrianclim <aclim@sfu.ca>
2016-01-27 14:02:23 -0800

    Testing Stuff

--- scripts/mauvewrap.py
+++ scripts/mauvewrap.py
@@ -4,8 +4,8 @@ import os
 #This can file can be moved to lib after testing
 #These paths can be moved to the settings file after testing
 MAUVE_PATH = "/home/vagrant/project/chef/cookbooks/baseconfig/files/data/Modules/iv-backend/islandviewer_dev/utils/mauve_2.4.0/linux-x64/"
-MAUVE_OUTPUT_PATH = "/vagrant/temp"
-MAUVE_SCRIPT_BASH_PATH = "/vagrant/islandviewer-ui/scripts/mauve-wrapper.sh"
+MAUVE_OUTPUT_PATH = "/home/alim/temp"
+MAUVE_SCRIPT_BASH_PATH = "/data/Modules/islandviewer5/islandviewer-ui/scripts"
 
 #Parameters = path to 2 genebank files
 #Returns None
