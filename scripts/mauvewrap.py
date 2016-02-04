42c8adda522c053b8c4f440cd74fd58ecfba3997
adrianclim <aclim@sfu.ca>
2016-02-04 10:04:12 -0800

    Replace output_path to one used in prod

--- scripts/mauvewrap.py
+++ scripts/mauvewrap.py
@@ -5,8 +5,8 @@ import os
 #This can file can be moved to lib after testing
 #These paths can be moved to the settings file after testing
 MAUVE_PATH = "/data/Modules/iv-backend/islandviewer_dev/utils/mauve_2.4.0/linux-x64"
-MAUVE_OUTPUT_PATH = "/home/alim/temp"
-MAUVE_SCRIPT_BASH_PATH = "/data/Modules/islandviewer5/islandviewer-ui/scripts/mauve-wrapper.sh"
+MAUVE_OUTPUT_PATH = "/data/Modules/iv-backend/islandviewer/pairwise_mauve"
+MAUVE_SCRIPT_BASH_PATH = "/data/Modules/islandviewer4/islandviewer-ui/scripts/mauve-wrapper.sh"
 
 #Parameters = path to 2 genebank files
 #Returns None
